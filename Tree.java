3cee05fddd03ec38528d39817d9c8970598efef3
Abdul Qadir Jakhro <aqjakhro0@gmail.com>
2024-10-29 16:28:15 +0500

    added in order function

--- Tree.java
+++ Tree.java
@@ -41,12 +41,29 @@ public class Tree{
 
         return root;
     }
+
+    //method to print tree in Order "L N R" left node right
+    static void inOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        //recursive call for left node
+        inOrder(root.left);
+        System.out.println(root.data);//print data, left most node
+        //recursive call for right node
+        inOrder(root.right);
+    }
 }
 /*
-explanation for upper code:- so i made a node class with left and right node, made a integer type data type for node's data;
+1- createTree():- so i made a node class with left and right node, made a integer type data type for node's data;
 In main class i created a static method to create a binary tree, in that method initially i just created an empty node, asked user for data, checked if it's null
 or not if yes then it'll stop at that position that means the recursion call is ended, then asked for left and right for the node recursively.
 the process will go on untill user puts "-1" in both left and right node/child nodes, when it's finally done for left nodes means left for last most node is "-1",
 then it'll ask for right of that node if right isn't null it'll recursively call the create tree function again, if right of that node is null too then it'll automatically 
 go to previous/parent node for it's right- this process is recursive and will go on-
+
+2- i created a method to print the binary tree in "left, node, right" order also known as inOrder. at first i checked if the root is null, if yes simple return, and if not
+recursive call the inOrder function and pass the root's left as parameter, it will go continuously untill node/root's left isn'nt null, then it'll print the left most node.
+Then it'll go for that left most node's right in "ln 55", and same thing goes for right too-
 */
